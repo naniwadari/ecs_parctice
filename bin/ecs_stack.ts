57648b90668738b7443f7c742357b42ddbe83a2b
Kouji Matsuno <matsunok@dialog-inc.com>
2024-11-11 18:49:32 +0900

    HTTPSで運用できるよう更新

--- bin/ecs_stack.ts
+++ bin/ecs_stack.ts
@@ -2,7 +2,10 @@
 import "source-map-support"
 import * as Cdk from "aws-cdk-lib"
 import { EcsStack } from "../stack/ecs_stack"
-import { config } from "../stack/config/test"
+
+const env = process.env.CDK_ENV
+const config = require("../stack/config/" + env).config
+console.log(`env: ${env} deploy start`)
 
 const app = new Cdk.App()
 new EcsStack(app, config.stackName, {
