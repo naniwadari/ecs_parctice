b8387ac760124e1e3e6f35db4314e398f7231271
Kouji Matsuno <matsunok@dialog-inc.com>
2024-11-12 21:18:39 +0900

    設定用の型ファイルを作成

--- stack/config/example.ts
+++ stack/config/example.ts
@@ -1,8 +1,9 @@
 import { RemovalPolicy } from "aws-cdk-lib"
 import * as Ec2 from "aws-cdk-lib/aws-ec2"
 import { AuroraMysqlEngineVersion } from "aws-cdk-lib/aws-rds"
+import { Config } from "./config";
 
-export const config = {
+export const config: Config = {
   account: "",
   region: "",
   resourceName: "",
@@ -16,13 +17,17 @@ export const config = {
     // お試しなのでスタック削除時はまとめて削除、本番では非推奨
     removalPolicy: RemovalPolicy.DESTROY,
   },
-  // ベースとなるドメインとホストゾーンIDと名称
-  route53HostedZoneID: "",
-  route53HostedZoneName: "", //ex. example.com
-  // サブドメインの名前
-  route53SubDomainName: "", 
-  // ALBで利用する証明書
-  certificateArn: "arn:aws:acm:ap-northeast-1:xxxxxxxxxx",
+  route53: {
+    // ベースとなるドメインとホストゾーンIDと名称
+    hostedZoneId: "",
+    hostedZoneName: "", //ex. example.com
+    // サブドメインの名前
+    subDomainName: "",
+  },
+  elb: {
+    // ALBで利用する証明書
+    certificateArn: "arn:aws:acm:ap-northeast-1:xxxxxxxxxx",
+  },
   rds: {
     removalPolicy: RemovalPolicy.DESTROY,
     // デフォルトデータベース名
